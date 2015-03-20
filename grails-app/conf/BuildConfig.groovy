deeb8f702b8c863f74304c97192394435c3e3e00
Jeff Bonhag <jeff@thebonhags.com>
2015-03-20 19:44:35 -0400

    Use Postgres in production

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,17 +12,17 @@ grails.project.source.level = 1.6
 //]
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
-        // excludes 'ehcache'
-    }
-    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+  // inherit Grails' default dependencies
+  inherits("global") {
+    // specify dependency exclusions here; for example, uncomment this to disable ehcache:
+    // excludes 'ehcache'
+  }
+  log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     checksums true // Whether to verify checksums on resolve
     legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
 
     repositories {
-        inherits true // Whether to inherit repository definitions from plugins
+      inherits true // Whether to inherit repository definitions from plugins
 
         grailsPlugins()
         grailsHome()
@@ -38,26 +38,27 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
 
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
+  dependencies {
+    // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
 
-        // runtime 'mysql:mysql-connector-java:5.1.22'
-    }
+    // runtime 'mysql:mysql-connector-java:5.1.22'
+    runtime 'postgresql:postgresql:8.4-702.jdbc3'
+  }
 
-    plugins {
-        runtime ":hibernate:$grailsVersion"
-        runtime ":jquery:1.8.3"
-        runtime ":resources:1.1.6"
+  plugins {
+    runtime ":hibernate:$grailsVersion"
+      runtime ":jquery:1.8.3"
+      runtime ":resources:1.1.6"
 
-        // Uncomment these (or add new ones) to enable additional resources capabilities
-        //runtime ":zipped-resources:1.0"
-        //runtime ":cached-resources:1.0"
-        //runtime ":yui-minify-resources:0.1.5"
+      // Uncomment these (or add new ones) to enable additional resources capabilities
+      //runtime ":zipped-resources:1.0"
+      //runtime ":cached-resources:1.0"
+      //runtime ":yui-minify-resources:0.1.5"
 
-        build ":tomcat:$grailsVersion"
+      build ":tomcat:$grailsVersion"
 
-        runtime ":database-migration:1.3.2"
+      runtime ":database-migration:1.3.2"
 
-        compile ':cache:1.0.1'
-    }
+      compile ':cache:1.0.1'
+  }
 }
